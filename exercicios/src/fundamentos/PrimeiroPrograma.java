61c502cf73403de3a481015304f6e946394515c9
KailanySousa <kailany-sousa2010@hotmail.com>
2021-05-24 23:31:16 -0300

    exemplo comentário JavaDoc
    exemplo da utilizando do JavaDoc para auxiliar na documentação do código com comentário

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -1,8 +1,18 @@
 package fundamentos;
 
+/**
+ * Essa classe representa... JavaDOC
+ * @author Kailany Sousa <kailanysousag@gmail.com>
+ * @since JDK1.0
+ * @see
+ *
+ */
 public class PrimeiroPrograma {
 	
-	// metódo principal, que inicia o sistema
+	/**
+	 * metódo principal, que inicia o sistema
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello Word!");
 	}
