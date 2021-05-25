f8e7c73b758e423dcfdf4856509a03f88d8b044b
KailanySousa <kailany-sousa2010@hotmail.com>
2021-05-24 23:16:57 -0300

    primeito programa

---
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,9 @@
+package fundamentos;
+
+public class PrimeiroPrograma {
+	
+	// metódo principal, que inicia o sistema
+	public static void main(String[] args) {
+		System.out.println("Hello Word!");
+	}
+}
