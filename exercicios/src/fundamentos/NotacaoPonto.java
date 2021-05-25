274836c8650158f11f48ff6d0c54fa81c6001a5b
KailanySousa <kailany-sousa2010@hotmail.com>
2021-05-25 18:23:37 -0300

    notação ponto

---
+++ exercicios/src/fundamentos/NotacaoPonto.java
@@ -0,0 +1,32 @@
+package fundamentos;
+
+public class NotacaoPonto {
+	public static void main(String[] args) {
+		double a = 2.3;
+		
+		/*
+		 * String  é um tipo não primitivo
+		 * não primitivo possui mais funções
+		 * */
+		String s = "Bom dia X";
+		
+		s = s.toUpperCase();
+		System.out.println(s);
+		
+		s = s.replace("X", "Senhora");
+		System.out.println(s);
+		
+		s = s.concat("!!!");
+		System.out.println(s);
+		
+		String x = "Bom dia X"
+				.replace("X", "kai")
+				.toUpperCase()
+				.concat("!!!");
+		
+		System.out.println(x);
+		
+		//Tipos primitivos não possuem o operador "."
+		
+	}
+}
