57f91e15d044b73971ebe7bff05ffa5ba102f8ab
KailanySousa <kailany-sousa2010@hotmail.com>
2021-05-27 23:10:45 -0300

    desafio aritmético

---
+++ exercicios/src/fundamentos/DesafioAritmeticos.java
@@ -0,0 +1,18 @@
+package fundamentos;
+
+public class DesafioAritmeticos {
+
+	public static void main(String[] args) {
+	
+		int exp1 = (int) Math.pow((3+2) * 6, 2) / (3*2);
+		
+		int exp2 = (int) Math.pow((1-5) * (2-7)/2, 2);
+		
+		int resultado = exp1 - exp2;
+		resultado = (int) Math.pow(resultado, 3) ;
+		resultado = (int) (resultado  / Math.pow(10, 3));
+		
+		System.out.println(resultado);
+	}
+
+}
