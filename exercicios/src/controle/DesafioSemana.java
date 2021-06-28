980d1046d2d81e5e68e24666e4de81715623e95b
KailanySousa <kailany-sousa2010@hotmail.com>
2021-06-28 19:47:51 -0300

    desafio dia da semana

---
+++ exercicios/src/controle/DesafioSemana.java
@@ -0,0 +1,32 @@
+package controle;
+
+import java.util.Scanner;
+
+public class DesafioSemana {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		String diaSemana = entrada.nextLine();
+		
+		if(diaSemana.toLowerCase().equals("domingo")) {
+			System.out.println("1");
+		} else if(diaSemana.toLowerCase().equals("segunda")) {
+			System.out.println("2");
+		} else if(diaSemana.toLowerCase().equals("terça")) {
+			System.out.println("3");
+		} else if(diaSemana.toLowerCase().equals("quarta")) {
+			System.out.println("4");
+		} else if(diaSemana.toLowerCase().equals("quinta")) {
+			System.out.println("5");
+		} else if(diaSemana.toLowerCase().equals("sexta")) {
+			System.out.println("6");
+		} else {
+			System.out.println("7");
+		}
+		
+		entrada.close();
+
+	}
+
+}
