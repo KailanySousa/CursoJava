dd91ddd9b9eda990466772e40ac77a1c8ff792ee
KailanySousa <kailany-sousa2010@hotmail.com>
2021-07-01 20:08:25 -0300

    for #2

--- exercicios/src/controle/For2.java
+++ exercicios/src/controle/For2.java
@@ -5,17 +5,8 @@ import java.util.Scanner;
 public class For2 {
 	public static void main(String[] args) {
 		
-		Scanner entrada = new Scanner(System.in);
-
-		String texto = "";
-		do {
-			System.out.println("Você precisa falar as palavras mágicas...");
-			System.out.println("Quer sair?");
-			texto = entrada.nextLine();
-		} while (!texto.equalsIgnoreCase(texto));
-		
-		System.out.println("Obrigada!");
-		
-		entrada.close();
+		for(int contador = 10; contador >=0; contador -= 2) {
+			System.out.println("Contador " + contador);
+		}
 	}
 }
