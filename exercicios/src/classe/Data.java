88ef4b15c1e49451bc0561ca803e96774491f72a
KailanySousa <kailany-sousa2010@hotmail.com>
2021-07-03 15:10:02 -0300

    desafio construtor

--- exercicios/src/classe/Data.java
+++ exercicios/src/classe/Data.java
@@ -4,6 +4,18 @@ public class Data {
 	int dia;
 	int mes;
 	int ano;
+	
+	Data(){
+		this.dia = 1;
+		this.mes = 1;
+		this.ano = 1970;
+	}
+	
+	Data(int dia, int mes, int ano){
+		this.dia = dia;
+		this.mes = mes;
+		this.ano = ano;
+	}
 
 	// metodo adicionado
 	String dataFormatoBR() {
