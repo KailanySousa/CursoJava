ea39fa4752ecb2a47ffbf79b62b90788374d3cfb
KailanySousa <kailany-sousa2010@hotmail.com>
2021-07-05 21:22:54 -0300

    variaveis locais e valores padrões

--- exercicios/src/classe/Data.java
+++ exercicios/src/classe/Data.java
@@ -5,13 +5,29 @@ public class Data {
 	int mes;
 	int ano;
 	
+	// quando uma constante é criada, 
+	// é necessário definir o seu valor
+	// até o objeto ser construido, ou seja,
+	// em um dos construtores da classe ou 
+	// no momento em que ela é criada
+	
 	Data(){
-//		this.dia = 1;
-//		this.mes = 1;
-//		this.ano = 1970;
+		// this.dia = 1;
+		// this.mes = 1;
+		// this.ano = 1970;
 		
 		// um construtor pode chamar outro construtor
 		this(1,1,1970);
+		
+		// valores padrões dos tipos primitivos
+		// quando o valor não é informado
+		// byte, short, int, long -> 0
+		// float, double -> 0
+		// boolean -> false
+		// char -> '\u0000'
+		
+		// por padrão o java não permite usar 
+		// uma variavel local sem inicializa-la antes
 	}
 	
 	Data(int dia, int mes, int ano){
@@ -27,6 +43,7 @@ public class Data {
 
 	// metodo adicionado
 	String dataFormatoUS() {
-		return String.format("%d-%d-%d", this.ano, this.mes, this.dia);
+		final String formato = "%d-%d-%d";
+		return String.format(formato, this.ano, this.mes, this.dia);
 	}
 }
