65b52e67344c83ec9bafbcf08284cfba8a7829a2
KailanySousa <kailany-sousa2010@hotmail.com>
2021-07-12 20:02:23 -0300

    Relacionamento um pra um

---
+++ exercicios/src/oo/composicao/Motor.java
@@ -0,0 +1,18 @@
+package oo.composicao;
+
+public class Motor {
+
+	final Carro carro;
+	boolean ligado = false;
+	double fatorInjecao = 1;
+
+	Motor (Carro carro){
+		this.carro = carro;
+	}
+	
+	int giros() {
+
+		return ligado ? (int) Math.round(fatorInjecao * 3000) : 0;
+
+	}
+}
