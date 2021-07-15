3fbdf952838f40b3d5baaee31ddea608ede71fa7
KailanySousa <kailany-sousa2010@hotmail.com>
2021-07-15 19:39:44 -0300

    modificadores de acesso
    private, package (default), protected e public

---
+++ exercicios/src/oo/composicao/encapsulamento/casaA/Paulo.java
@@ -0,0 +1,20 @@
+package oo.composicao.encapsulamento.casaA;
+
+public class Paulo {
+
+	Ana esposa = new Ana();
+
+	void testeAcessos() {
+		// retorna erro, pois o atributo segredo está como privado
+		// System.out.println(esposa.segredo); 
+
+		// é possível acessar pois está no mesmo pacote
+		System.out.println(esposa.facoDentroDeCasa);
+		
+		// é possível acessar pois está no mesmo pacote
+		System.out.println(esposa.formaDeFalar);
+		
+		// é possível acessar pois está publico
+		System.out.println(esposa.todosSabem);
+	}
+}
---
+++ exercicios/src/oo/composicao/encapsulamento/casaB/Carlos.java
@@ -0,0 +1,29 @@
+package oo.composicao.encapsulamento.casaB;
+
+import oo.composicao.encapsulamento.casaA.Ana;
+
+public class Carlos extends Ana{
+	
+	
+	void testeAcessos() {
+		// retorna erro, pois o atributo segredo está como privado
+		// System.out.println(mae.segredo); 
+
+		// não é possível acessar pois está em pacotes diferentes
+		// System.out.println(mae.facoDentroDeCasa);
+		
+		/* 
+		 	quando um atributo está como protected, não é possível
+			acessar ele pela instancia do objeto
+			basta chama-lo diretamente, pois pela herança
+			classe atual pode chamar diretamente
+		*/
+		
+		// Ana mae = new Ana();
+		// System.out.println(mae.formaDeFalar);
+		System.out.println(formaDeFalar);
+		
+		// é possível acessar pois está publico
+		System.out.println(todosSabem);
+	}
+}
