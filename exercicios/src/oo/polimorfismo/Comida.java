e9a60cd036d5d126233587d5a36ef6ef6844383d
KailanySousa <kailany-sousa2010@hotmail.com>
2021-07-15 21:15:10 -0300

    polimorfismo dinamico
    criação de uma classe generica para que as especificas herdem os atributos e metodos dela e assim um metodo poderá receber como parametro tudo que for herdado da classe generica, independentemente do tipo

--- exercicios/src/oo/polimorfismo/Comida.java
+++ exercicios/src/oo/polimorfismo/Comida.java
@@ -1,10 +1,10 @@
 package oo.polimorfismo;
 
-public class Pessoa {
-
+public class Comida {
+	
 	private double peso;
 
-	public Pessoa(double peso) {
+	public Comida(double peso) {
 		this.peso = peso;
 	}
 
@@ -15,10 +15,6 @@ public class Pessoa {
 	public void setPeso(double peso) {
 		if (peso >= 0)
 			this.peso = peso;
-	}
-	
-	public void comer(Comida comida) {
-		this.peso += comida.getPeso();
 	}
 	
 }
