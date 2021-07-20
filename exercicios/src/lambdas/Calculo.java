cd9f3de43e73fa71249e10b5a22a1b9ac1c5b591
KailanySousa <kailany-sousa2010@hotmail.com>
2021-07-19 21:43:43 -0300

    interfaces funcionais #01

--- exercicios/src/lambdas/Calculo.java
+++ exercicios/src/lambdas/Calculo.java
@@ -1,5 +1,8 @@
 package lambdas;
 
+// anotação para usar a interface no contexto de lambda
+// sendo assim, só poderá ter um metódo
+@FunctionalInterface
 public interface Calculo {
 
 	public abstract double executar(double a, double b);
