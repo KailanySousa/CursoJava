70860c95e60d1d6e57c12cb1ad992f8936e93e45
KailanySousa <kailany-sousa2010@hotmail.com>
2021-07-29 19:13:50 -0300

    Outros Métodos Stream

--- exercicios/src/streams/Aluno.java
+++ exercicios/src/streams/Aluno.java
@@ -21,4 +21,38 @@ public class Aluno {
 	public String toString() {
 		return this.nome + " tem nota " + this.nota;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(nota);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + presenca;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (Double.doubleToLongBits(nota) != Double.doubleToLongBits(other.nota))
+			return false;
+		if (presenca != other.presenca)
+			return false;
+		return true;
+	}
+	
 }
