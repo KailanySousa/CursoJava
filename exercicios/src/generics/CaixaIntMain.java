11cfe69e00f4ee9cb105ec8f4985c353c8c00f88
KailanySousa <kailany-sousa2010@hotmail.com>
2021-07-30 19:21:34 -0300

    exemplos com generics

---
+++ exercicios/src/generics/CaixaIntMain.java
@@ -0,0 +1,15 @@
+package generics;
+
+public class CaixaIntMain {
+
+	public static void main(String[] args) {
+		
+		CaixaInt caixaA = new CaixaInt();
+		caixaA.guardar(123);
+		
+		Integer coisaA = caixaA.abrir();
+		System.out.println(coisaA);
+
+	}
+
+}
