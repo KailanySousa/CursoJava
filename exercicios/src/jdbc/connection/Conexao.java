13f3141cbcc121fb1ebb81422561862c6b48fec7
KailanySousa <kailany-sousa2010@hotmail.com>
2021-08-05 16:45:03 -0300

    externalizando dados de conexao

--- exercicios/src/jdbc/connection/Conexao.java
+++ exercicios/src/jdbc/connection/Conexao.java
@@ -1,23 +1,35 @@
 package jdbc.connection;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Conexao {
 
 	public static Connection getConexao() {
 		
 		try {
-			String url = "jdbc:mysql://localhost:3306/curso_java?verifyServerCertificate=false&useSSL=true";
-			String usuario = "root";
-			String senha = "140975";
+			Properties prop = getProperties();
+			String url = prop.getProperty("db.url");
+			String usuario = prop.getProperty("db.user");
+			String senha = prop.getProperty("db.pass");
 			Connection conexao = DriverManager.getConnection(url, usuario, senha);
 		
 			return conexao;
-		} catch (SQLException e) {
+		} catch (SQLException | IOException e) {
 			throw new RuntimeException(e);
 		}
 		
+	}
+	
+	private static Properties getProperties() throws IOException {
+		Properties prop = new Properties();
+		
+		String caminho = "/conexao.properties";
+		prop.load(Conexao.class.getResourceAsStream(caminho));
+		
+		return prop;
 	}
 }
