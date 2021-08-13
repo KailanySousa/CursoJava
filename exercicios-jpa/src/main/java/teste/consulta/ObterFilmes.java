690cb0ed88a39fea4d7b1d98def51fa7796599e1
KailanySousa <kailany-sousa2010@hotmail.com>
2021-08-13 20:18:44 -0300

    JPA: consulta nomeada/ dao

---
+++ exercicios-jpa/src/main/java/teste/consulta/ObterFilmes.java
@@ -0,0 +1,22 @@
+package teste.consulta;
+
+import java.util.List;
+
+import infra.DAO;
+import modelo.muitospramuitos.Filme;
+
+public class ObterFilmes {
+
+	public static void main(String[] args) {
+		
+		DAO<Filme> dao = new DAO<>();
+		
+		List<Filme> filme = dao.consultar("obterFilmesComNotaMaiorQue", "nota", 8,5);
+		
+		for(Filme f: filmes) {
+			System.out.println(f.getNome());
+		}
+
+	}
+
+}
