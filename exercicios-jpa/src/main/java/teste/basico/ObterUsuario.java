75484e3bf6f6429ae11e2633e27d4ae574d8e141
KailanySousa <kailany-sousa2010@hotmail.com>
2021-08-13 16:19:38 -0300

    JPA: obter usuario

--- exercicios-jpa/src/main/java/teste/basico/ObterUsuario.java
+++ exercicios-jpa/src/main/java/teste/basico/ObterUsuario.java
@@ -6,23 +6,19 @@ import javax.persistence.Persistence;
 
 import modelo.basico.Usuario;
 
-public class NovoUsuario {
+public class ObterUsuario {
 
 	public static void main(String[] args) {
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exercicios-jpa");
 		EntityManager em = emf.createEntityManager();
-
-		Usuario novoUsuario = new Usuario("Maria", "maria@teste.com");
 		
-		em.getTransaction().begin();
-		em.persist(novoUsuario);
-		em.getTransaction().commit();
-		
-		System.out.println("O id gerado foi: " + novoUsuario.getId());
+		Usuario u = em.find(Usuario.class, 2L); 
+		System.out.println(u.getNome());
 		
 		em.close();
 		emf.close();
+
 	}
 
 }
