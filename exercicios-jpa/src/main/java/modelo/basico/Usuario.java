e9995291dafd1f52632de413dd90d7d6c627664d
KailanySousa <kailany-sousa2010@hotmail.com>
2021-08-13 16:01:06 -0300

    configurando projeto/criando entidade usuario

---
+++ exercicios-jpa/src/main/java/modelo/basico/Usuario.java
@@ -0,0 +1,54 @@
+package modelo.basico;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+
+@Entity
+public class Usuario {
+
+	@Id
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+	@Column(nullable = false)
+	private String email;
+	
+	@Transient //marcar o atributo para não ser criado na tabela do banco de dados
+	private boolean status;
+	
+	public Usuario() {}
+	
+	public Usuario(String nome, String email) {
+		this.nome = nome;
+		this.email = email;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
