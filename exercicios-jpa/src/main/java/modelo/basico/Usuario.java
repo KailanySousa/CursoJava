1db555d6801e7ada081829afd183a365f002927f
KailanySousa <kailany-sousa2010@hotmail.com>
2021-08-13 16:16:00 -0300

    JPA: novo usuário

--- exercicios-jpa/src/main/java/modelo/basico/Usuario.java
+++ exercicios-jpa/src/main/java/modelo/basico/Usuario.java
@@ -2,6 +2,8 @@ package modelo.basico;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Transient;
 
@@ -9,6 +11,7 @@ import javax.persistence.Transient;
 public class Usuario {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	@Column(nullable = false)
