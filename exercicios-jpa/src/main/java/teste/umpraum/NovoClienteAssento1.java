acac2d28a6e1f5c345cf091901d10c7bbe02e527
KailanySousa <kailany-sousa2010@hotmail.com>
2021-08-13 19:43:03 -0300

    JPA: Um pra um/ dao #01

---
+++ exercicios-jpa/src/main/java/teste/umpraum/NovoClienteAssento1.java
@@ -0,0 +1,24 @@
+package teste.umpraum;
+
+import infra.DAO;
+import modelo.umpraum.Assento;
+import modelo.umpraum.Cliente;
+
+public class NovoClienteAssento1 {
+
+	public static void main(String[] args) {
+	
+		Assento a = new Assento("16c");
+		Cliente c = new Cliente("Ana", a);
+		
+		DAO<Object> dao = new DAO<>();
+		
+		dao.abrirT()
+			.incluir(a)
+			.incluir(c)
+			.fecharT()
+			.fechar();;
+
+	}
+
+}
