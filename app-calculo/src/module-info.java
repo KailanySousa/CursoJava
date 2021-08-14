ce1e832c0ceefdba667c7b1a560570e2f204f9f8
KailanySousa <kailany-sousa2010@hotmail.com>
2021-08-14 18:35:12 -0300

    modularidade: exports to

--- app-calculo/src/module-info.java
+++ app-calculo/src/module-info.java
@@ -2,4 +2,6 @@ module app.calculo {
 	
 	exports br.com.ksg.app.calculo;
 	requires transitive app.logging;
+	
+	exports br.com.ksg.app.calculo.interno to app.financeiro;
 }
--- app-financeiro/src/br/com/ksg/app/financeiro/Teste.java
+++ app-financeiro/src/br/com/ksg/app/financeiro/Teste.java
@@ -1,6 +1,7 @@
 package br.com.ksg.app.financeiro;
 
 import br.com.ksg.app.calculo.Calculadora;
+import br.com.ksg.app.calculo.interno.OperacoesAritmeticas;
 
 public class Teste {
 
@@ -8,7 +9,9 @@ public class Teste {
 		
 		Calculadora c = new Calculadora();
 		System.out.println(c.soma(2, 3 ,4));
-
+		
+		OperacoesAritmeticas op = new OperacoesAritmeticas();
+		System.out.println(op.soma(5, 6, 7));
 	}
 
 }
