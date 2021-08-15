a3f12fb04b3c6e67a2d1637a1ae2667c56381c9f
KailanySousa <kailany-sousa2010@hotmail.com>
2021-08-15 17:01:40 -0300

    JavaFX; TilePane

--- exercicios-jfx/src/layout/Quadrado.java
+++ exercicios-jfx/src/layout/Quadrado.java
@@ -1,11 +1,5 @@
 package layout;
 
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.control.Label;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
@@ -17,13 +11,13 @@ public class Quadrado extends Rectangle {
 	private String[] cores = { "#c33c5e", "#39aac6", "#28d79a", "#fb750e", "#6657a8", "#f9060e" };
 
 	public Quadrado() {
-		this(100, 100);
+		this(100);
 	}
 
-	public Quadrado(int largura, int altura) {
+	public Quadrado(int tamanho) {
 		
-		setHeight(altura);
-		setWidth(altura);
+		setHeight(tamanho);
+		setWidth(tamanho);
 		
 		setFill(Color.web(this.cores[i]));
 		
