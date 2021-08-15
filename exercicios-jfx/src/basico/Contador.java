c2b0d5c014388e49433dcdadc320346d3e7c70c0
KailanySousa <kailany-sousa2010@hotmail.com>
2021-08-15 14:10:57 -0300

    JavaFX: contador #02

--- exercicios-jfx/src/basico/Contador.java
+++ exercicios-jfx/src/basico/Contador.java
@@ -11,38 +11,49 @@ import javafx.stage.Stage;
 
 public class Contador extends Application {
 
+	private int contador = 0;
+
 	public static void main(String[] args) {
 		launch(args);
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		
+
 		Label lblTitulo = new Label("Contador");
 		Label lblNumero = new Label("0");
-		
 		Button btnDecremento = new Button("-");
 		Button btnIncremento = new Button("+");
+
+		btnDecremento.setOnAction(e -> {
+			this.contador--;
+			lblNumero.setText(Integer.toString(this.contador));
+		});
 		
+		btnIncremento.setOnAction(e -> {
+			this.contador++;
+			lblNumero.setText(Integer.toString(this.contador));
+		});
+
 		VBox boxPrincipal = new VBox();
 		boxPrincipal.setSpacing(10);
 		boxPrincipal.setAlignment(Pos.CENTER);
 		boxPrincipal.getChildren().add(lblTitulo);
 		boxPrincipal.getChildren().add(lblNumero);
-	
+
 		HBox boxSecundario = new HBox();
 		boxSecundario.setSpacing(10);
 		boxSecundario.setAlignment(Pos.CENTER);
 		boxSecundario.getChildren().add(btnDecremento);
 		boxSecundario.getChildren().add(btnIncremento);
-		
+
 		boxPrincipal.getChildren().add(boxSecundario);
-		
+
 		Scene cena = new Scene(boxPrincipal, 400, 400);
-		
+
 		primaryStage.setScene(cena);
 		primaryStage.show();
-		
+
 	}
 
 }
