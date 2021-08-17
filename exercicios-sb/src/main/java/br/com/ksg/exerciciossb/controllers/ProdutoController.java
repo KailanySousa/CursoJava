b18d1f16ca2b668c7ddc8fb5067866dc535c0c39
KailanySousa <kailany-sousa2010@hotmail.com>
2021-08-17 18:43:08 -0300

    Spring Boot: consulta por id

--- exercicios-sb/src/main/java/br/com/ksg/exerciciossb/controllers/ProdutoController.java
+++ exercicios-sb/src/main/java/br/com/ksg/exerciciossb/controllers/ProdutoController.java
@@ -1,9 +1,12 @@
 package br.com.ksg.exerciciossb.controllers;
 
+import java.util.Optional;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,7 +22,7 @@ public class ProdutoController {
 	@Autowired
 	private ProdutoRepository produtoRepository;
 
-	@PostMapping("/inserir")
+	@PostMapping
 	public @ResponseBody Produto novoProduto(@Valid Produto produto) {
 
 		produtoRepository.save(produto);
@@ -30,5 +33,10 @@ public class ProdutoController {
 	@GetMapping
 	public Iterable<Produto> obterProdutos() {
 		return produtoRepository.findAll();
+	}
+	
+	@GetMapping(path = "/{id}")
+	public Optional<Produto> obterProdutoPorId(@PathVariable int id) {
+		return produtoRepository.findById(id);
 	}
 }
