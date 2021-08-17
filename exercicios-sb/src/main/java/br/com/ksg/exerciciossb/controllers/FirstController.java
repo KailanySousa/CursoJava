ba5d44e69e7ce0116866ee7d58219d392dbd149c
KailanySousa <kailany-sousa2010@hotmail.com>
2021-08-17 16:16:56 -0300

    Spring Boot: passagem de parametros p/ webservice

--- exercicios-sb/src/main/java/br/com/ksg/exerciciossb/controllers/FirstController.java
+++ exercicios-sb/src/main/java/br/com/ksg/exerciciossb/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package br.com.ksg.exerciciossb.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,13 +14,18 @@ public class FirstController {
 		return "Olá Spring Boot";
 	}
 	
-	@RequestMapping(method = RequestMethod.GET, path = "tudo_bem")
+	@GetMapping(path = "tudo_bem")
 	public String tudoBem() {
 		return "Tudo bem?";
 	}
 	
 	@GetMapping(path = {"ola", "saudacao"})
 	public String saudacao() {
+		return "Olá, Tudo bem?";
+	}
+	
+	@PostMapping(path = "saudacao")
+	public String saudacaoPost() {
 		return "Olá, Tudo bem?";
 	}
 }
